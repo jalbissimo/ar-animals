6356a12bd25ef1bada5bd9952bcf201563824559
Mihai Jalba <j_mihai_97@yahoo.com>
2023-11-30 21:30:55 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import ReactArApp from "./ReactArApp";
+
+function App() {
+	return (
+		<div className="App">
+			<ReactArApp />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/ReactArApp.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { AFrameRenderer, Marker } from "react-web-ar";
+
+export default function ReactArApp() {
+	const [increment, setIncrement] = useState(0);
+
+	var i = 0;
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setIncrement(i++);
+		}, 10);
+		return () => clearInterval(interval);
+	}, [i]);
+	return (
+		<AFrameRenderer inherent={true}>
+			<Marker
+				parameters={{
+					preset: "pattern",
+					type: "pattern",
+					patternUrl: "https://raw.githubusercontent.com/lbelfield/augmented-reality/master/src/components/trainTicket/train-ticket.patt",
+					url: "https://raw.githubusercontent.com/lbelfield/augmented-reality/master/src/components/trainTicket/train-ticket.patt",
+				}}
+			>
+				<a-gltf-model src="https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Fox/glTF/Fox.gltf" rotation={"0 0" + increment} scale="0.01 0.01 0.01"></a-gltf-model>
+			</Marker>
+			<Marker parameters={{ preset: "kanji" }}>
+				<a-entity>
+					<a-gltf-model src="https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Models/master/2.0/Duck/glTF/Duck.gltf" rotation={"0 0" + increment} scale="1 1 1"></a-gltf-model>
+				</a-entity>
+			</Marker>
+		</AFrameRenderer>
+	);
+}
